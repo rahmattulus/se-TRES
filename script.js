86df78315badfe1b08d8e3243e396df1f144aaf4
Joshua <joshuauta123@gmail.com>
2024-09-25 20:19:14 +0700

    Co-authored-by: ELsiPemula <ElNaThan27@users.noreply.github.com> Co-authored-by: rahmattullus <rahmattulus@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.scrollY >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
