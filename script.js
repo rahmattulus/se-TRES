a3a178daca32d9cfe75f45519d3d15cf5bc42860
rahmattulus <rahmattuluz01@gmail.com>
2024-09-30 15:57:20 +0700

    yaya

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function () {
+//   myFunction();
+// };
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+
+const title = document.getElementById("moving-title");
+const scrollPosition = window.scrollY;
+
+// Adjust the transform value to create an upward motion effect
+title.style.transform = `translateY(${Math.min(scrollPosition, 70)}px)`;
 
 // function myFunction() {
 //   if (window.scrollY >= sticky) {
